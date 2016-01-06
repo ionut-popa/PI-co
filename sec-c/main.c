75f9e18dd9e2525c58af633c95eeb4fdb3283af2
Ionut Popa <ionut.popa@windriver.com>
2016-01-06 11:55:57 +0200

    Add C initial impl

---
+++ sec-c/main.c
@@ -0,0 +1,460 @@
+/*
+ * Fast LZ-SEC compression
+ */
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+
+typedef unsigned char byte;
+
+
+typedef struct _Match {
+    int pos;
+    int len;
+} Match;
+
+
+typedef struct _ICodec {
+    byte    *buffer;
+    int     bufferLen;
+    int     currentByte;
+    int     currentBitPos;
+    int     currentBytePos;
+} ICodec;
+
+
+int window                              = (2 * 1024);
+int lookahead                           = 16;
+int lookaheadBits                       = 4;
+int windowBits                          = 11;
+
+int  monogramCount[256]                 = { 0 };
+int  monogramIndexAdd[256]              = { 0 };
+int  monogramIndexRemove[256]           = { 0 };
+int  monogramPositions[256][8 * 1024]   = { 0 };
+
+
+
+static inline int log2int(int val) {
+//    if (val == 0) return 1000000;
+//    if (val == 1) return 0;
+//    int ret = 0;
+//    while (val >= 1) {
+//        val >>= 1;
+//        ret++;
+//    }
+//    return ret;
+    return ceil(log2(val));
+}
+
+/////////////////////////////////////////////////////////////////////////////////////
+// BitStream encoding / decoding
+/////////////////////////////////////////////////////////////////////////////////////
+
+            //0  1  2  3  4   5   6   7    8
+int mask[] = {0, 1, 3, 7, 15, 31, 63, 127, 255};
+
+
+void decodeInit(ICodec *decoder) {
+    decoder->currentByte = 0;
+    decoder->currentBitPos = 8;
+    memset(monogramCount, 0, 256 * sizeof(int));
+    memset(monogramIndexAdd, 0, 256 * sizeof(int));
+    memset(monogramIndexRemove, 0, 256 * sizeof(int));
+    memset(monogramPositions, 0, 256 * window * sizeof(int));
+}
+
+
+inline long decodeInteger(ICodec *decoder, int totalBits) {
+    long result = 0;
+    int offset = 0;
+    if (totalBits < 0) {
+        return 0;
+    }
+
+    int currentBitPos   = decoder->currentBitPos;
+    int currentBytePos  = decoder->currentBytePos;
+    int currentByte     = decoder->currentByte;
+
+    do {
+        if (totalBits < currentBitPos) {
+            result |= (currentByte & mask[totalBits]) << offset;
+            currentBitPos -= totalBits;
+            currentByte >>= totalBits;
+            totalBits = 0;
+            break;
+        } else {
+            result |= (currentByte & mask[currentBitPos]) << offset;
+            totalBits -= currentBitPos;
+            offset += currentBitPos;
+            currentBitPos -= currentBitPos;
+            currentByte >>= currentBitPos;
+
+                currentBytePos++;
+                //if (currentBytePos < decoder->bufferLen) {
+                    currentByte = decoder->buffer[currentBytePos] & 0xFF;
+                //}
+                currentBitPos = 8;
+        }
+//        if (currentBitPos == 0) {
+//            currentBytePos++;
+//            if (currentBytePos < decoder->bufferLen) {
+//                currentByte = decoder->buffer[currentBytePos] & 0xFF;
+//            }
+//            currentBitPos = 8;
+//        }
+    } while (totalBits > 0);
+
+    decoder->currentBitPos  = currentBitPos;
+    decoder->currentBytePos = currentBytePos;
+    decoder->currentByte    = currentByte;
+
+    return result;
+}
+
+
+void encodeInit(ICodec *encoder) {
+    encoder->currentByte = 0;
+    encoder->currentBitPos = 0;
+    memset(monogramCount, 0, 256 * sizeof(int));
+    memset(monogramIndexAdd, 0, 256 * sizeof(int));
+    memset(monogramIndexRemove, 0, 256 * sizeof(int));
+    memset(monogramPositions, 0, 256 * window * sizeof(int));
+}
+
+
+void encodeInteger(ICodec *encoder, int val, int totalBits) {
+    //printf("Code: %d/%d\n", val, totalBits);
+    do {
+        encoder->currentByte |= ((val << encoder->currentBitPos) & 0xFF);
+        if (encoder->currentBitPos + totalBits > 8) {
+            totalBits -= (8 - encoder->currentBitPos);
+            val >>= (8 - encoder->currentBitPos);
+            encoder->currentBitPos += (8 - encoder->currentBitPos);
+        } else {
+            encoder->currentBitPos += totalBits;
+            val >>= totalBits;
+            totalBits = 0;
+        }
+
+        if (encoder->currentBitPos == 8) {
+            encoder->buffer[encoder->currentBytePos++] = (byte) encoder->currentByte;
+            encoder->currentBitPos = 0;
+            encoder->currentByte = 0;
+        }
+    } while (totalBits > 0);
+}
+
+
+byte * encodeFlush(ICodec *encoder) {
+    if (encoder->currentBytePos != 0) {
+        encoder->buffer[encoder->currentBytePos++] = (byte) encoder->currentByte;
+    }
+    return encoder->buffer;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+Match findMatch(byte input[], int startSearchPosition, int maxLenght, int _window, int sync) {
+    int maxLenghtFound      = -1;
+    int posMaxLenghtFound   = 0;
+    int p, j;
+
+    if (startSearchPosition > 0) {
+        int monogramStart = input[startSearchPosition];
+        //printf("Monogram count:%d, monogramStart: %d\n", monogramCount[monogramStart], monogramStart);
+        for (p = 0; p < monogramCount[monogramStart]; p++) {
+            int i = monogramPositions[monogramStart][(monogramIndexRemove[monogramStart] + p ) % window];
+            if (i > startSearchPosition - maxLenght) {
+                break;
+            }
+
+            for (j = 0; j < maxLenght; j++) {
+                if (input[i + j] == input[startSearchPosition + j]) {
+                    if (j > maxLenghtFound) {
+                        maxLenghtFound = j;
+                        if (sync == 1) {
+                            posMaxLenghtFound = p;
+                        } else {
+                            posMaxLenghtFound = i;
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    Match m = {posMaxLenghtFound, maxLenghtFound + 1};
+    return m;
+}
+
+
+static byte* compress3(byte input[], int inputLength, ICodec *encoder)  {
+    int len = 0, i;
+
+    int position = 0;
+
+    //first - encodeInteger file length
+    byte b1 = (byte) (inputLength >> 24);
+    byte b2 = (byte) ((inputLength << 8) >> 24);
+    byte b3 = (byte) ((inputLength << 16) >> 24);
+    byte b4 = (byte) ((inputLength << 24) >> 24);
+
+    encodeInteger(encoder, b1 & 0xFF, 8);
+    encodeInteger(encoder, b2 & 0xFF, 8);
+    encodeInteger(encoder, b3 & 0xFF, 8);
+    encodeInteger(encoder, b4 & 0xFF, 8);
+
+    //Block format:
+    // [Match length]{If Match length > 0}{[sync][Match pos]}[Literal]
+
+    while (position < inputLength - lookahead- 1) {
+        int oldPosition = position;
+
+        int m = input[position];
+
+        Match match1 = findMatch(input, position, lookahead - 1, window, 0);
+        //int[] match2 = new int[2];int sync = 0;///Length:5936749
+        Match match2 = findMatch(input, position - 1, lookahead, window - 1, 1);int sync = 1;
+
+        double score1 = (log2(lookahead) + 1 + log2(window) + 8) / match1.len;
+        double score2 = (log2(lookahead) + 1 + log2(monogramCount[m]) + 8) / (match2.len - 1);
+
+        if (match1.len <= 0) {
+            score1 = 10000000;
+        }
+        if (match2.len <= 2) {
+            score2 = 10000000;
+        }
+
+        if (score1 <= score2) {
+            int oldLen = len;
+            int MatchPos = match1.pos;
+            if (position - window < 0 || match1.len == 0) {
+                // Nothing
+            } else {
+                MatchPos = MatchPos - (position - window);
+            }
+
+            if (match1.len != 0) {
+                len += log2(lookahead) + sync + log2(window) + 8 ;
+                encodeInteger(encoder, match1.len /*Match length*/, lookaheadBits);
+                if (sync > 0) {
+                    encodeInteger(encoder, 0 /*sync*/, 1);
+                }
+                encodeInteger(encoder, MatchPos /*Match pos*/, windowBits);
+            } else {
+                len += log2(lookahead) + 8;
+                encodeInteger(encoder, match1.len /*Match length*/, lookaheadBits);
+            }
+            position += match1.len;
+            encodeInteger(encoder, input[position] & 0xFF /*literal*/, 8);
+            position += 1;
+        } else {
+            m = input[position - 1];
+            int oldLen = len;
+
+            len += log2(lookahead - 1) + sync + log2(monogramCount[m]) + 8;
+
+            int MatchPos = match2.pos;
+
+            encodeInteger(encoder, match2.len - 1 /*Match length*/, lookaheadBits);
+            encodeInteger(encoder, 1 /*sync*/, 1);
+            encodeInteger(encoder, MatchPos /*Match size*/, (int) log2int(monogramCount[m] - 1));
+            position += match2.len - 1;
+            encodeInteger(encoder, input[position] & 0xFF /*literal*/, 8);
+            position += 1;
+        }
+        // If position > window
+        //      Iterate from oldPosition - window to position - window
+        //          Remove from monogramCount all positions
+        //              monogramIndexRemove++
+
+        // Iterate from oldPosition -> position
+        //      Add to monogramCount all the new positions
+        //      Add to monogramIndexAdd++
+        //      Add to monogramPositions[indexAdd] -> the position
+        //      Add to monogramPositionsInverse[the position] -> indexAdd
+        for (i = (oldPosition - window) < 0 ? 0 : (oldPosition - window);
+                i < (position - window);
+                i++) {
+            int mm = input[i];
+            monogramCount[mm]--;
+            monogramPositions[mm][monogramIndexRemove[mm]] = -1;
+            monogramIndexRemove[mm] = (monogramIndexRemove[mm] + 1) % window;
+        }
+        for (i = (oldPosition) < 0 ? 0 : (oldPosition);
+                i < (position);
+                i++) {
+            int mm = input[i];
+            monogramCount[mm]++;
+            monogramPositions[mm][monogramIndexAdd[mm]] = i;
+            monogramIndexAdd[mm] = (monogramIndexAdd[mm] + 1) % window;
+        }
+    }
+    //add a padding value to the end -> this will act as a barrier for the decoding process - allowing us to remove some extra checks
+    encodeInteger(encoder, 0x00, 8);
+    return encodeFlush(encoder);
+}
+
+
+static byte* decompress3(ICodec *decoder, int *bufferLen) {
+    int i;
+
+    //read original input length
+    int b1 = (int) decodeInteger(decoder, 8);
+    int b2 = (int) decodeInteger(decoder, 8);
+    int b3 = (int) decodeInteger(decoder, 8);
+    int b4 = (int) decodeInteger(decoder, 8);
+
+    int outputLength = (b1 << 24) | (b2 << 16) | (b3 << 8) | b4;
+    *bufferLen = outputLength;
+
+    //System.out.println("OUTPUT: " + outputLength);
+
+    byte* output = (byte *) malloc(outputLength);
+
+    int position = 0;
+    while (position < outputLength) {
+        int oldPosition = position;
+
+        //Block format:
+        // [Match length]{If Match length > 0}{[sync][Match pos]}[Literal]
+
+        int length = (int) decodeInteger(decoder, lookaheadBits);
+        if (length == 0) {
+            //read
+            int literal = (int) decodeInteger(decoder, 8);
+            output[position] = (byte) (literal & 0xFF);
+            position++;
+        } else {
+            //read sync
+            int sync = (int) decodeInteger(decoder, 1);
+            if (sync == 0) {
+                int matchPos = (int) decodeInteger(decoder, windowBits);
+                if (position - window < 0) {
+                    // nothing
+                } else {
+                    matchPos = position - window + matchPos;
+                }
+                for (i = 0; i < length; i++) {
+                    output[position] = output[matchPos + i];
+                    position++;
+                }
+            } else {
+                int m = output[position - 1];
+                int matchPos = (int) decodeInteger(decoder, (int) ceil(log2(monogramCount[m] - 1)));
+                length += 1;
+                matchPos = monogramPositions[m][(monogramIndexRemove[m] + matchPos) % window];
+
+                //position = position;
+                for (i = 1; i < length; i++) {
+                    output[position] = output[matchPos + i];
+                    position++;
+                }
+            }
+            int literal = (int) decodeInteger(decoder, 8);
+            output[position] = (byte) (literal & 0xFF);
+            position++;
+        }
+
+        for (i = (oldPosition - window) < 0 ? 0 : (oldPosition - window);
+                i < (position - window);
+                i++) {
+            int mm = output[i];
+            monogramCount[mm]--;
+            monogramPositions[mm][monogramIndexRemove[mm]] = -1;
+            monogramIndexRemove[mm] = (monogramIndexRemove[mm] + 1) % window;
+        }
+        for (i = (oldPosition) < 0 ? 0 : (oldPosition);
+                i < (position);
+                i++) {
+            int mm = output[i];
+            monogramCount[mm]++;
+            monogramPositions[mm][monogramIndexAdd[mm]] = i;
+            monogramIndexAdd[mm] = (monogramIndexAdd[mm] + 1) % window;
+        }
+    }
+
+    return output;
+}
+
+
+int main(int argc, char *argv[]) {
+    int fd, offset;
+    byte *data;
+    struct stat sbuf;
+
+    if (argc != 4) {
+        fprintf(stderr, "usage: %s [-c|-d] <file-in> <file-out>\n", argv[0]);
+        exit(1);
+    }
+
+    if ((fd = open(argv[2], O_RDONLY)) == -1) {
+        perror("open");
+        exit(1);
+    }
+
+    if (stat(argv[2], &sbuf) == -1) {
+        perror("stat");
+        exit(1);
+    }
+
+    data = mmap((caddr_t) 0, sbuf.st_size, PROT_READ, MAP_SHARED, fd, 0);
+
+    if (data == (caddr_t)(-1)) {
+        perror("mmap");
+        exit(1);
+    }
+
+    printf("Argc: %s", argv[1]);
+    if (strcmp("-c", argv[1]) == 0) {
+        // Initialize the encoder and allocate space for compressed data buffer
+        ICodec encoder = { 0 };
+        encoder.bufferLen = sbuf.st_size;
+        encoder.buffer = (byte *) malloc(encoder.bufferLen);
+        encodeInit(&encoder);
+
+        printf("Start encoding\n");
+        compress3(data, sbuf.st_size, &encoder);
+        printf("End encoding: %d\n", encoder.currentBytePos);
+
+        FILE *out = fopen(argv[3], "wb");
+
+        if (out){
+            fwrite(encoder.buffer, encoder.currentBytePos, 1, out);
+        }
+
+        fclose(out);
+    }
+    if (strcmp("-d", argv[1]) == 0) {
+        ICodec decoder = { 0 };
+        decoder.bufferLen = sbuf.st_size;
+        decoder.buffer = data;
+
+        decodeInit(&decoder);
+        printf("Start encoding\n");
+        int bufferLen;
+        byte *buffer = decompress3(&decoder, &bufferLen);
+        printf("End decoding");
+        FILE *out = fopen(argv[3], "wb");
+
+        if (out){
+            fwrite(buffer, bufferLen, 1, out);
+        }
+
+        fclose(out);
+    }
+}
